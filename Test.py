2b6d703982a0257535b02aa49857bf9cad35807b
Matvey Morozov <34199760+MatveyMor@users.noreply.github.com>
2020-04-03 14:51:39 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+import time
+
+start_time = time.time()
+
+for i in range(10):
+    print(i, 'Matvey')
+    
+    
+end_time = time.time()
+
+print('General time:', end_time - start_time)
+
+
+# In[ ]:
+
+
+
+
